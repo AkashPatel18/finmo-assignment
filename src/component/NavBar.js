15cb0c64ca6a0cfd55945e134cc6182f03088e43
Akash <akashpatel18041999@gmail.com>
2022-04-10 08:45:52 +0530

    feat: added boilerplate

---
+++ src/component/NavBar.js
@@ -0,0 +1,10 @@
+import { AppBar } from "@mui/material";
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <AppBar position="static" className="navbar">
+      Shopsey
+    </AppBar>
+  );
+};
