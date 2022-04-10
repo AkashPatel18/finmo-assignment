8f1b71f7a89729e2a9c07f497483fa0ebc3d60cf
Akash <akashpatel18041999@gmail.com>
2022-04-10 13:53:56 +0530

    feat : added login signup page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,6 +1,62 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="loginContainer">
+      <div>
+        <h3>Login Here</h3>
+      </div>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+          password: Yup.string()
+            .min(8, "Password is too short - should be 8 chars minimum.")
+            .max(16, "Password is too long - should be 8 chars maximum.")
+            .matches(/[a-zA-Z]/, "Password can only contain Latin letters.")
+            .required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <div className="loginForm">
+          <Form
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              minWidth: "300px",
+            }}
+          >
+            <label htmlFor="email">Email Address</label>
+            <Field name="email" type="email" className={"field"} />
+            <ErrorMessage name="email">
+              {(msg) => <div className="error">{msg}</div>}
+            </ErrorMessage>
+
+            <label htmlFor="password">Password</label>
+            <Field name="password" type="text" className={"field"} />
+            <ErrorMessage name="password">
+              {(msg) => <div className="error">{msg}</div>}
+            </ErrorMessage>
+
+            <button type="submit" className="loginBtn">
+              Submit
+            </button>
+          </Form>
+        </div>
+      </Formik>
+      <p>
+        New User ? <Link to={"/register"}>Register</Link>
+      </p>
+    </div>
+  );
 };
