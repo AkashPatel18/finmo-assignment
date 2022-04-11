79e8dc207ddf99b0ddee10563d281a5803a20dff
Akash <akashpatel18041999@gmail.com>
2022-04-11 10:14:56 +0530

    feat : added table

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,15 +1,71 @@
-import { Container } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 
 export const Cart = () => {
   const { cartItems, totalPrice } = useSelector((state) => state.products);
 
-  console.log(totalPrice);
+  const absoluteProducts = {};
+
+  for (let item of cartItems) {
+    if (absoluteProducts[item.id]) {
+      absoluteProducts[item.id] = {
+        ...item,
+        ...{ count: absoluteProducts[item.id].count + 1 },
+      };
+    } else {
+      absoluteProducts[item.id] = {
+        ...item,
+        ...{ count: 1 },
+      };
+    }
+  }
+
+  const items = Object.values(absoluteProducts);
+
+  const handleRemove = () => {};
+
   return (
     <Container fixed style={{ marginTop: 80 }}>
-      <div style={{ border: "1px solid red" }}>
-        <p></p>
+      <div>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Action</th>
+          </tr>
+          {items.map((item, key) => {
+            return (
+              <tr key={key}>
+                <td>{item.title.slice(0, 16)}</td>
+                <td>{item.price}</td>
+                <td>{item.count}</td>
+                <td onClick={handleRemove}>remove</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginTop: 100,
+        }}
+      >
+        <p>Total Price : {totalPrice}</p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button style={{ background: "#fb641b", color: "white" }}>
+          Check out
+        </Button>
       </div>
     </Container>
   );
